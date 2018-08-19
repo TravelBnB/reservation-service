4f7a5469e85445d74f4da42aa038f66ade640224
Shane Yuan <shaneyuan18@gmail.com>
2018-08-18 18:23:06 -0700

    refactor redis

---
+++ server/redisHelper.js
@@ -0,0 +1,57 @@
+const redis = require('redis');
+
+const REDIS_URL = process.env.REDIS_URL;
+const client = redis.createClient(REDIS_URL);
+
+const redisHash = (res, req, ratesKey, callback) => {
+  // client.incr(countKey, (err, count) => {
+  client.hgetall(ratesKey, (err, rates) => {
+    if (rates) {
+      return res.send(rates);
+    }
+    callback(req.params, (err, result) => {
+      if (err) {
+        res.status(500).send({ err: `Server oopsie ${err}` });
+      } else if (result.length === 0) {
+        res.status(404).send('No such listing');
+      } else {
+        client.hmset(
+          ratesKey, result.rows[0], (err, result) => {
+            if (err) console.log(err);
+          },
+        );
+        return res.send(result.rows[0]);
+      }
+    });
+  });
+  // });
+};
+
+const redisSet = (res, req, ratesKey, callback) => {
+// client.incr(countKey, (err, count) => {
+  client.get(ratesKey, (err, rates) => {
+    if (rates) {
+      return res.send(JSON.parse(rates));
+    }
+    callback(req.params, (err, result) => {
+      if (err) {
+        res.status(500).send({ err: `Server oopsie ${err}` });
+      } else if (result.length === 0) {
+        res.status(404).send('No such listing');
+      } else {
+        client.set(
+          ratesKey, JSON.stringify(result.rows), (err, result) => {
+            if (err) console.log(err);
+          },
+        );
+        return res.send(result.rows);
+      }
+    });
+  });
+// });
+};
+
+module.exports = {
+  redisHash,
+  redisSet,
+};
--- server/server.js
+++ server/server.js
@@ -1,116 +1,65 @@
 const newRelic = require('newrelic');
 const compression = require('compression');
+const cluster = require('cluster');
 const express = require('express');
-const redis = require('redis');
 const path = require('path');
 const db = require('../postgresdb/db.js');
-const utils = require('./utils.js');
-const axios = require('axios');
-
-const API_URL = 'http://api.fixer.io';
+const redisHelper = require('./redisHelper');
 
 // change Num sockets
 // var http = require('http');
 // http.globalAgent.maxSockets = 25;
 
 // Redis
-const REDIS_URL = process.env.REDIS_URL;
-const client = redis.createClient(REDIS_URL);
+// const REDIS_URL = process.env.REDIS_URL;
+// const client = redis.createClient(REDIS_URL);
 
 const PORT = process.env.PORT || 3003;
-
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.listen(PORT, () => console.log('Listening at port: ' + PORT));
-
 
 app.get('/listings/:listingId', (req, res) => {
   const listingId = req.params.listingId;
-  const countKey = `${listingId}:count`;
-  const ratesKey = `${listingId}:rates`;
-  const url = `${API_URL}/${listingId}`;
-
-  client.incr(countKey, (err, count) => {
-    client.hgetall(ratesKey, (err, rates) => {
-      if (rates) {
-        return res.send(rates);
-      }
-      db.getListingById(req.params, (err, result) => {
-        if (err) {
-          res.status(500).send({ err: `Server oopsie ${err}` });
-        } else if (result.length === 0) {
-          res.status(404).send('No such listing');
-        } else {
-          const { total_reviews, avg_rating } = result.rows[0];
-          result.rows[0].reviews = { total_reviews, avg_rating };
-          client.hmset(
-            ratesKey, result.rows[0], (err, result) => {
-              if (err) console.log(err);
-            },
-          );
-          return res.send({ 
-            count,
-            rates: result.rows[0],
-          });
-        }
-      });
-    });
+  // const countKey = `lists:${listingId}:count`;
+  const ratesKey = `lists:${listingId}:rates`;
+  redisHelper.redisHash(res, req, ratesKey, (input, callback) => {
+    db.getListingById(input, callback);
   });
-  // db.getListingById(req.params, (err, result) => {
-  //   if (err) {
-  //     res.status(500).send({ err: `Server oopsie ${err}` });
-  //   } else if (result.length === 0) {
-  //     res.status(404).send('No such listing');
-  //   } else {
-  //     const { total_reviews, avg_rating } = result.rows[0];
-  //     result.rows[0].reviews = { total_reviews, avg_rating };
-  //     // res.json({ rates: res.data.rates });
-  //     console.log(result.rows[0]);
-  //     res.send(result.rows[0]);
-  //   }
-  // });
 });
 
 app.get('/listings/:listingId/reservations', (req, res) => {
   // TODO: refactor using router
-  const data = req.params.listingId;
-  db.getBookedDatesByListingId(data, (err, result) => {
-    if (err) {
-      res.status(500).send({ err: `Server oopsie ${err}` });
-    } else {
-      res.send(result.rows);
-    }
+  const listingId = req.params.listingId;
+  // const countKey = `reservations:lists:${listingId}:count`;
+  const ratesKey = `reservations:lists:${listingId}:rates`;
+  redisHelper.redisSet(res, req, ratesKey, (input, callback) => {
+    db.getBookedDatesByListingId(input, callback);
   });
 });
 
 app.get('/guests/:guestId/reservations', (req, res) => {
   // TODO: refactor using router
-  const data = req.params.guestId;
-  db.getBookedDatesByGuestId(data, (err, result) => {
-    if (err) {
-      res.status(500).send({ err: `Server oopsie ${err}` });
-    } else {
-      res.send(result.rows);
-    }
+  const guestId = req.params.listingId;
+  // const countKey = `reservations:guests:${guestId}:count`;
+  const ratesKey = `reservations:guests:${guestId}:rates`;
+  redisHelper.redisSet(res, req, ratesKey, (input, callback) => {
+    db.getBookedDatesByGuestId(input, callback);
   });
 });
 
 app.get('/listings/:listingId/guests/:guestId/reservations', (req, res) => {
   // TODO: refactor using router
   const { listingId, guestId } = req.params;
-  db.getBookedDatesByListingGuestId(listingId, guestId, (err, result) => {
-    if (err) {
-      res.status(500).send({ err: `Server oopsie ${err}` });
-    } else {
-      res.send(result.rows);
-    }
+  // const countKey = `reservations:lists:${listingId}:guests:${guestId}:count`;
+  const ratesKey = `reservations:lists:${listingId}:guests:${guestId}:rates`;
+  redisHelper.redisSet(res, req, ratesKey, (input, callback) => {
+    db.getBookedDatesByListingGuestId(input, callback);
   });
 });
-
 
 app.post('/reservations', (req, res) => {
   // TODO: find more elegant implementation that ensures atomicity
